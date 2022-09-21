785e0117b5a68fb4e1a0ccab32296113188605e4
soliops <106052857+soliops@users.noreply.github.com>
2022-09-21 18:19:43 +0900

    Add files via upload

--- python/py1/bef6.py
+++ python/py1/bef6.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup 
 from requests import *
-
+#해당 사이트에 접속하여 Devtool로 볼 경우 데이터가 확인이 되지만
+#실제 크롤링 후 스크래핑시 데이터가 확인이 안될 경우 AJAX 및 JavaScript로 
+#직접 태그가 생성되도록 제작되었음. 이럴경우 스크래핑하기가 어려워짐
 url="https://www.koreabaseball.com/TeamRank/TeamRank.aspx"
 result = get(url)
 # print(result.raise_for_status())
